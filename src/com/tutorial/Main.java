1028f211a9f4161d39c944e9ca96a917d12137a7
raffiMRG <mutekinoraffi@gmail.com>
2023-07-08 12:06:01 +0700

    feature: waktuHitung

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -7,15 +7,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        long waktuStart, waktuFinish;
+
         // membuka file
         FileInputStream fileInput = new FileInputStream("input.txt");
 
         // membaca file
+        waktuStart = System.nanoTime();
         int data = fileInput.read();
         while(data != -1){
             System.out.print((char) data);
             data = fileInput.read();
         }
+
+        waktuFinish = System.nanoTime();
+        // menampilkan lama waktu
+        System.out.println("\n\nwaktu : " + (waktuFinish - waktuStart));
 
         // menutup file
         fileInput.close();
